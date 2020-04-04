44a1ff7b65402f143d4a29199d8f3900c56d4157
Ramiro Filho <ramirofilho18@gmail.com>
2020-04-03 22:12:49 -0300

    versão 1

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,27 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		File file = new File("c:\\eclipse\\temp\\in.txt");
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		String path = "c:\\eclipse\\temp\\in.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = br.readLine();
 			}
+			
 		}
 		catch(IOException e){
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {
-			if(sc != null) {
-			sc.close();
-			}
-		}
+		
 
 	}
 
